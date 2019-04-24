3f92d4d7d1acccfedd81ef876006f23997b6e9cf
echocho <echo333cheng@gmail.com>
2019-04-25 01:03:23 +0800

    stored timestamp in utc

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Message(db.Model):
     __tablename__ = 'mb_message'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    created_at = db.Column(db.DateTime, default=datetime.now)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     content = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(20), nullable=False)
