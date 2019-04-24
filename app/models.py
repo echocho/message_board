8c48a6d51df97fca882f6963f70dd641827799bf
echocho <echo333cheng@gmail.com>
2019-04-24 17:47:36 +0800

    Message model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-# from sqlalchemy import Base, Column, String
 from app import db
 from datetime import datetime
 
 class Message(db.Model):
+    __tablename__ = 'mb_message'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
     content = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(20), nullable=False)
