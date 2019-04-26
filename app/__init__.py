f7545c364782633c63572852670e7ce9441c32b5
echocho <echo333cheng@gmail.com>
2019-04-27 01:28:14 +0800

    bootstrap instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy 
@@ -11,5 +12,6 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 moment = Moment(app)
+bootstrop = Bootstrap(app)
 
 from app import views
